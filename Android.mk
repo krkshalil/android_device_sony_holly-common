5786bf2d1149b6f7d154952827f9bd75c165591c
krkshalil <krkshalil@outlook.com>
2016-12-18 14:42:21 +0000

    Some Fİxes

--- Android.mk
+++ Android.mk
@@ -1,3 +1,7 @@
+LOCAL_PATH := $(device/sony/holly-common/mtk)
+
 # Hack for build
 $(shell mkdir -p $(OUT)/obj/SHARED_LIBRARIES/libminui_intermediates)
-$(shell touch $(OUT)/obj/SHARED_LIBRARIES/libminui_intermediates/export_includes) 
+$(shell touch $(OUT)/obj/SHARED_LIBRARIES/libminui_intermediates/export_includes)
+ 
+include device/sony/holly-common/mtk/Android.mk
